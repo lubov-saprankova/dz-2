42a5a28857022bd049934ebc65bc296a76a66f13
Lubov Saprankova'git config --global user.email 'Lubysik-999@mail.ru'git config --global user.name Lubov <Lubysik-999@mail.ru>
2022-05-26 16:28:44 +0300

    dz2

--- src/main/java/printThreeWords.java
+++
@@ -1,7 +0,0 @@
-public class printThreeWords {
-    public static void main(String[] args) {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-}
