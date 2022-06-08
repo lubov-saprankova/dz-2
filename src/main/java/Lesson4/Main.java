f409bf34e59aea2c5e4b77e81eb7a968ff8175b9
Lubov Saprankova'git config --global user.email 'Lubysik-999@mail.ru'git config --global user.name Lubov <Lubysik-999@mail.ru>
2022-06-08 17:28:52 +0300

    Lesson4

--- src/main/java/Lesson4/Main.java
+++ src/main/java/Lesson4/Main.java
@@ -1,353 +1,111 @@
 package Lesson4;
-
 import java.util.Random;
 import java.util.Scanner;
-
 public class Main {
-    /**
-     * Крестики-нолики в процедурном стиле
-     *
-     * 1. Полностью разобраться с кодом, попробовать переписать с нуля, стараясь
-     * не подглядывать в методичку;
-     *
-     * 2. Переделать проверку победы, чтобы она не была реализована просто набором условий,
-     * например, с использованием циклов.
-     *
-     * 3. Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и
-     * количества фишек 4. Очень желательно не делать это просто набором условий для каждой из
-     * возможных ситуаций;
-     *
-     * 4. Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока.
-     */
-    public class Dz4 {
-        private static char[][] tiles;
-        private static final int SIZE = 3;
-        private static final int ACTION_SUM = SIZE * SIZE;
-        private static final int DOTS_TO_WIN = SIZE;
-        private static final char DOT_EMPTY = '_';
-        private static final char DOT_X = 'X';
-        private static final char DOT_O = 'O';
-        private static Scanner scanner = new Scanner(System.in);
+    public static int SIZE = 5;
+    public static int DOTS_TO_WIN = 5;
 
-        private static final Random random = new Random();
-        private static boolean trigger = true;
-        private static int actionCounter = 0;
-        private static char currentDot;
-
-        public static void main(String[] args) {
-            fillMap();
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static char[][] map;
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
             printMap();
-
-            while (checkGameLoop()) {
-                if (trigger) {
-                    currentDot = DOT_X;
-//                humanPlayer(); // метод для человека
-//                stupidPcPlayer(); // для pc vs. pc
-                    aiPlayer(); // метод не смог реализовать
-                } else {
-                    currentDot = DOT_O;
-//                stupidPcPlayer(); // pc
-                    aiPlayer(); // метод не смог реализовать
-                }
-
-                trigger = !trigger;
-                printMap();
-                actionCounter++;
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
             }
         }
-
-        private static void fillMap() {
-            tiles = new char[SIZE][SIZE];
-
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    tiles[i][j] = DOT_EMPTY;
-                }
+        System.out.println("Игра закончена");
+    }
+    public static boolean checkWin(char symb) {
+        // horizontal winner checkup
+        if(map[0][0] == symb && map[0][1] == symb && map[0][2] == symb && map[0][3] == symb && map[0][4] == symb) return true;
+        if(map[1][0] == symb && map[1][1] == symb && map[1][2] == symb && map[1][3] == symb && map[1][4] == symb) return true;
+        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb && map[2][3] == symb && map[2][4] == symb) return true;
+        if (map[3][0] == symb && map[3][1] == symb && map[3][2] == symb && map[3][3] == symb && map[3][4] == symb) return true;
+        if (map[4][0] == symb && map[4][1] == symb && map[4][2] == symb && map[4][3] == symb && map[4][4] == symb) return true;
+        //  diagonal winner checkup
+        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb && map[3][3] == symb && map[4][4] == symb) return true;
+        //vertical winner checkup
+        if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb && map[3][0] == symb && map[4][0] == symb) return true;
+        if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb && map[3][1] == symb && map[4][1] == symb) return true;
+        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb && map[3][2] == symb && map[4][2] == symb) return true;
+        if (map[0][3] == symb && map[1][3] == symb && map[2][3] == symb && map[3][3] == symb && map[4][3] == symb) return true;
+        if (map[0][4] == symb && map[1][4] == symb && map[2][4] == symb && map[3][4] == symb && map[4][4] == symb) return true;
+        return false;
+    }
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
             }
         }
-
-        private static void printMap() {
-            System.out.printf("   ");
-            for (int i = 0; i < SIZE; i++) {
-                System.out.printf("%2d ", i + 1);
+        return true;
+    }
+    public static void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        map[y][x] = DOT_O;
+    }
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X -диаг. Y-вертик.");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        map[y][x] = DOT_X;
+    }
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
             }
-
+        }
+    }
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
             System.out.println();
-
-            for (int i = 0; i < SIZE; i++) {
-                System.out.printf("%2d ", i + 1);
-                for (int j = 0; j < SIZE; j++) {
-                    System.out.printf("%2c ", tiles[i][j]);
-                }
-                System.out.println();
-            }
-
-            System.out.println();
         }
-
-        private static int[] humanInput() {
-            int input[] = new int[2];
-
-            while (!scanner.hasNextInt()) {
-                scanner.next();
-            }
-
-            while (!scanner.hasNextInt()) {
-                scanner.next();
-            }
-
-            input[0] = scanner.nextInt() - 1;
-            input[1] = scanner.nextInt() - 1;
-
-            return input;
-        }
-
-        private static void humanPlayer() {
-            int y;
-            int x;
-
-            do {
-                System.out.printf("Ходит человек [%c]. Введите Y X: ", currentDot);
-                int[] input = humanInput();
-                y = input[0];
-                x = input[1];
-            } while (!checkAction(y, x));
-
-            tiles[y][x] = currentDot;
-        }
-
-        private static void stupidPcPlayer() {
-            int y;
-            int x;
-
-            System.out.printf("Ходит AI [%c]", currentDot);
-
-            do {
-                y = random.nextInt(SIZE);
-                x = random.nextInt(SIZE);
-            } while (!checkAction(y, x));
-
-            System.out.printf(" (y: %d, x: %d)\n", y + 1, x + 1);
-
-            tiles[y][x] = currentDot;
-        }
-
-        private static boolean checkAction(int y, int x) {
-            if (x < 0 || y < 0 || x >= SIZE || y >= SIZE) return false;
-            if (DOT_EMPTY == tiles[y][x]) return true;
-            return false;
-        }
-
-        private static boolean isContinue() {
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    if (findRightUp(i, j)) return false;
-                    if (findRight(i, j)) return false;
-                    if (findRightDown(i, j)) return false;
-                    if (findDown(i, j)) return false;
-                }
-            }
-            return true;
-        }
-
-        private static boolean findRightUp(int y, int x) {
-            for (int j = 0; j < DOTS_TO_WIN; j++) {
-
-                if (y < 0 || y >= SIZE || x < 0 || x >= SIZE || currentDot != tiles[y][x]) return false;
-
-                y--;
-                x++;
-            }
-            return true;
-        }
-
-        private static boolean findRight(int y, int x) {
-            for (int j = 0; j < DOTS_TO_WIN; j++) {
-
-                if (y < 0 || y >= SIZE || x < 0 || x >= SIZE || currentDot != tiles[y][x]) return false;
-
-                x++;
-            }
-            return true;
-        }
-
-        private static boolean findRightDown(int y, int x) {
-            for (int j = 0; j < DOTS_TO_WIN; j++) {
-
-                if (y < 0 || y >= SIZE || x < 0 || x >= SIZE || currentDot != tiles[y][x]) return false;
-
-                y++;
-                x++;
-            }
-            return true;
-        }
-
-        private static boolean findDown(int y, int x) {
-            for (int j = 0; j < DOTS_TO_WIN; j++) {
-
-                if (y < 0 || y >= SIZE || x < 0 || x >= SIZE || currentDot != tiles[y][x]) return false;
-
-                y++;
-            }
-            return true;
-        }
-
-        private static boolean checkGameLoop() {
-            if (actionCounter == ACTION_SUM) return false;
-            if (!isContinue()) return false;
-            return true;
-        }
-
-        // Все ниже методы созданы для написания AI метода
-
-        private static void aiPlayer() {
-
-            switch (DOTS_TO_WIN) {
-                case 1:
-                case 2:
-                case 3:
-                    if (!isSet()) {
-                        stupidPcPlayer();
-                    }
-                    break;
-                default:
-                    stupidPcPlayer();
-                    break;
-            }
-        }
-
-        private static boolean isSet() {
-            System.out.println("AI");
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    if (setRightUp(i, j)) return true;
-                    if (setRight(i, j)) return true;
-                    if (setRightDown(i, j)) return true;
-                    if (setDown(i, j)) return true;
-                }
-            }
-            return false;
-        }
-
-        private static boolean setRightUp(int y, int x) {
-            int lastEmptyY = -1;
-            int lastEmptyX = -1;
-            int enemyCount = 0;
-            int emptyCount = 0;
-            char enemyDot = trigger ? DOT_O : DOT_X;
-
-            for (int j = 0; j < DOTS_TO_WIN; j++) {
-
-                if (y < 0 || y >= SIZE || x < 0 || x >= SIZE) return false;
-                if (DOT_EMPTY == tiles[y][x]) {
-                    lastEmptyY = y;
-                    lastEmptyX = x;
-                    emptyCount++;
-                }
-                if (enemyDot == tiles[y][x]) enemyCount++;
-                if (DOT_EMPTY == tiles[y][x]) emptyCount++;
-
-                y--;
-                x++;
-            }
-
-            if (lastEmptyY < 0 || lastEmptyX < 0 || lastEmptyY >= SIZE || lastEmptyX >= SIZE) {
-                System.out.println("error");
-                return false;
-            }
-
-            if (emptyCount > 0 && enemyCount >= SIZE - 1 && enemyCount > 0) tiles[lastEmptyY][lastEmptyX] = currentDot;
-            return true;
-        }
-
-        private static boolean setRight(int y, int x) {
-            int lastEmptyY = -1;
-            int lastEmptyX = -1;
-            int enemyCount = 0;
-            int emptyCount = 0;
-            char enemyDot = trigger ? DOT_O : DOT_X;
-
-            for (int j = 0; j < DOTS_TO_WIN; j++) {
-
-                if (y < 0 || y >= SIZE || x < 0 || x >= SIZE) return false;
-                if (DOT_EMPTY == tiles[y][x]) {
-                    lastEmptyY = y;
-                    lastEmptyX = x;
-                    emptyCount++;
-                }
-                if (enemyDot == tiles[y][x]) enemyCount++;
-                if (DOT_EMPTY == tiles[y][x]) emptyCount++;
-
-                x++;
-            }
-
-            if (lastEmptyY < 0 || lastEmptyX < 0 || lastEmptyY >= SIZE || lastEmptyX >= SIZE) {
-                System.out.println("error");
-                return false;
-            }
-
-            if (emptyCount > 0 && enemyCount >= SIZE - 1 && enemyCount > 0) tiles[lastEmptyY][lastEmptyX] = currentDot;
-            return true;
-        }
-
-        private static boolean setRightDown(int y, int x) {
-            int lastEmptyY = -1;
-            int lastEmptyX = -1;
-            int enemyCount = 0;
-            int emptyCount = 0;
-            char enemyDot = trigger ? DOT_O : DOT_X;
-
-            for (int j = 0; j < DOTS_TO_WIN; j++) {
-
-                if (y < 0 || y >= SIZE || x < 0 || x >= SIZE) return false;
-                if (DOT_EMPTY == tiles[y][x]) {
-                    lastEmptyY = y;
-                    lastEmptyX = x;
-                    emptyCount++;
-                }
-                if (enemyDot == tiles[y][x]) enemyCount++;
-                if (DOT_EMPTY == tiles[y][x]) emptyCount++;
-
-                y++;
-                x++;
-            }
-
-            if (lastEmptyY < 0 || lastEmptyX < 0 || lastEmptyY >= SIZE || lastEmptyX >= SIZE) {
-                System.out.println("error");
-                return false;
-            }
-
-            if (emptyCount > 0 && enemyCount >= SIZE - 1 && enemyCount > 0) tiles[lastEmptyY][lastEmptyX] = currentDot;
-            return true;
-        }
-
-        private static boolean setDown(int y, int x) {
-            int lastEmptyY = -1;
-            int lastEmptyX = -1;
-            int enemyCount = 0;
-            int emptyCount = 0;
-            char enemyDot = trigger ? DOT_O : DOT_X;
-
-            for (int j = 0; j < DOTS_TO_WIN; j++) {
-
-                if (y < 0 || y >= SIZE || x < 0 || x >= SIZE) return false;
-                if (DOT_EMPTY == tiles[y][x]) {
-                    lastEmptyY = y;
-                    lastEmptyX = x;
-                    emptyCount++;
-                }
-                if (enemyDot == tiles[y][x]) enemyCount++;
-                if (DOT_EMPTY == tiles[y][x]) emptyCount++;
-
-                y++;
-            }
-
-            if (lastEmptyY < 0 || lastEmptyX < 0 || lastEmptyY >= SIZE || lastEmptyX >= SIZE) {
-                System.out.println("error");
-                return false;
-            }
-
-            if (emptyCount > 0 && enemyCount >= SIZE - 1 && enemyCount > 0) tiles[lastEmptyY][lastEmptyX] = currentDot;
-            return true;
-        }
-    }}
+        System.out.println();
+    }
+}
